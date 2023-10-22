7aafb4a6d8d8798b3967b034b12f572620b5f901
iAmSyntax <65483968+iAmSyntax@users.noreply.github.com>
2023-10-22 16:22:28 +0530

    singleton database

--- app/src/main/java/com/example/roomapp/data/UserDatabase.kt
+++ app/src/main/java/com/example/roomapp/data/UserDatabase.kt
@@ -1,4 +1,33 @@
 package com.example.roomapp.data
 
-class UserDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class UserDatabase : RoomDatabase() {
+
+        abstract fun userDao():UserDao
+
+        companion object{
+            @Volatile
+            private var INSTANCE: UserDatabase? = null // rights to this field are made immediately visible
+
+            fun getDatabase(context:Context):UserDatabase{
+                    val tempInstance = INSTANCE
+                if(tempInstance != null){
+                    return tempInstance;
+                }
+                synchronized(this){
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        UserDatabase::class.java,
+                        "user_data"
+                    ).build()
+                    INSTANCE = instance // use singleton , a single instance of database is very efficient
+                    return  instance;
+                }
+            }
+        }
 }
