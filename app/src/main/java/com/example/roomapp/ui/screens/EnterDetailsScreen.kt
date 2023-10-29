f88f1be13ab816c05593fa739bf91d376072f2f7
iAmSyntax <65483968+iAmSyntax@users.noreply.github.com>
2023-10-29 12:39:06 +0530

    Removed Enter Details Text

--- app/src/main/java/com/example/roomapp/ui/screens/EnterDetailsScreen.kt
+++ app/src/main/java/com/example/roomapp/ui/screens/EnterDetailsScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,7 +30,7 @@ fun EnterDetailsScreen(navController: NavController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Enter Details")
+
         }
     }
 }
