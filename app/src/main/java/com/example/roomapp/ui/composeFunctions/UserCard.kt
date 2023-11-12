2c8c41f28193acefa59906f3e92f9588077afa2e
iAmSyntax <65483968+iAmSyntax@users.noreply.github.com>
2023-11-12 12:16:17 +0530

    Composable Function to fetch details

--- app/src/main/java/com/example/roomapp/ui/composeFunctions/UserCard.kt
+++ app/src/main/java/com/example/roomapp/ui/composeFunctions/UserCard.kt
@@ -5,14 +5,33 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.roomapp.data.User
+import com.example.roomapp.data.UserViewModel
 
 @Composable
-fun UserDetailsCard() {
+fun UserDetailsList(userViewModel: UserViewModel) {
+    val userData: List<User> by userViewModel.readAllData.observeAsState(listOf())
+
+    LazyColumn(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        items(userData.size) { index ->
+            UserDetailsCard(user = userData[index])
+            Spacer(modifier = Modifier.height(16.dp)) // Add space between cards
+        }
+    }
+}
+
+@Composable
+fun UserDetailsCard(user: User) {
     Card(
         modifier = Modifier
             .padding(
@@ -27,22 +46,22 @@ fun UserDetailsCard() {
             modifier = Modifier.padding(16.dp)
         ) {
 
-            Text("ID: 2")
+            Text("ID: ${user.id}")
             Spacer(modifier = Modifier.height(8.dp))
 
-            Text("First Name: Jamal")
+            Text("First Name: ${user.firstName}")
             Spacer(modifier = Modifier.height(8.dp))
 
-            Text("Last Name: Shameem")
+            Text("Last Name: ${user.lastName}")
             Spacer(modifier = Modifier.height(8.dp))
 
-            Text("Age: 24")
+            Text("Age: ${user.age}")
             Spacer(modifier = Modifier.height(8.dp))
 
-            Text("Position: Striker")
+            Text("Position: ${user.position}")
             Spacer(modifier = Modifier.height(8.dp))
 
-            Text("Rating: 23")
+            Text("Rating: ${user.rating}")
         }
     }
 }
