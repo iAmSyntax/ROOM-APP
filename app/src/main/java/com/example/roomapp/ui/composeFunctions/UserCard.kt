f9e94f7f376927db9b0a3f69e44bb4173c3af431
iAmSyntax <65483968+iAmSyntax@users.noreply.github.com>
2023-11-12 17:03:08 +0530

    Navigate from card of each user

--- app/src/main/java/com/example/roomapp/ui/composeFunctions/UserCard.kt
+++ app/src/main/java/com/example/roomapp/ui/composeFunctions/UserCard.kt
@@ -1,5 +1,6 @@
 package com.example.roomapp.ui.composeFunctions
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,25 +14,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.roomapp.data.User
 import com.example.roomapp.data.UserViewModel
+import com.example.roomapp.ui.route.Screen
 
 @Composable
-fun UserDetailsList(userViewModel: UserViewModel) {
+fun UserDetailsList(userViewModel: UserViewModel, navController: NavController) {
     val userData: List<User> by userViewModel.readAllData.observeAsState(listOf())
 
     LazyColumn(
         modifier = Modifier.padding(8.dp)
     ) {
         items(userData.size) { index ->
-            UserDetailsCard(user = userData[index])
+            UserDetailsCard(user = userData[index],navController)
             Spacer(modifier = Modifier.height(16.dp)) // Add space between cards
         }
     }
 }
 
 @Composable
-fun UserDetailsCard(user: User) {
+fun UserDetailsCard(user: User,navController: NavController) {
     Card(
         modifier = Modifier
             .padding(
@@ -41,6 +44,12 @@ fun UserDetailsCard(user: User) {
                 bottom = 16.dp
             ) // Adjust the top padding as needed
             .fillMaxWidth()
+            .clickable {
+                navController.navigate(route = Screen.UpdateDetails.route){
+
+                    navController.navigate(route = Screen.UpdateDetails.route)
+                }
+            }
     ) {
         Column(
             modifier = Modifier.padding(4.dp)
