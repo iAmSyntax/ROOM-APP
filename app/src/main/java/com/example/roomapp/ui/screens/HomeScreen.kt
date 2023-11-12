65a9c3254f0177afb48a3c4c52bb5e70cb816a9a
iAmSyntax <65483968+iAmSyntax@users.noreply.github.com>
2023-11-12 12:17:26 +0530

    Insertion of Fetch details composable function

--- app/src/main/java/com/example/roomapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/roomapp/ui/screens/HomeScreen.kt
@@ -6,21 +6,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.roomapp.data.UserViewModel
 import com.example.roomapp.ui.composeFunctions.CustomFloatingActionButton
 import com.example.roomapp.ui.composeFunctions.RoomAppBar
-import com.example.roomapp.ui.composeFunctions.UserDetailsCard
+import com.example.roomapp.ui.composeFunctions.UserDetailsList
 import com.example.roomapp.ui.route.Screen
 
 @Composable
 fun HomeScreen(navController: NavController) {
+    val userViewModel:UserViewModel = viewModel()
     Box(
         modifier = Modifier
             .background(Color.White)
             .fillMaxSize()
     ) {
         RoomAppBar()
-        UserDetailsCard()
+        UserDetailsList(userViewModel = userViewModel)
         CustomFloatingActionButton(
             onClick = {
                 // Handle button click
