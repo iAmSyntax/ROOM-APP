a42863fabd04478bbb9da2001192164ab5a04d6b
iAmSyntax <65483968+iAmSyntax@users.noreply.github.com>
2023-11-19 15:04:47 +0530

    update in user repository

--- app/src/main/java/com/example/roomapp/data/UserRepository.kt
+++ app/src/main/java/com/example/roomapp/data/UserRepository.kt
@@ -5,10 +5,14 @@ import androidx.lifecycle.LiveData
 //Repository class abstracts access to multiple data source
 class UserRepository(private val userDao: UserDao) {
 
-    val readAllData: LiveData<List<User>> = userDao.readAllUser();
+    val readAllData: LiveData<List<User>> = userDao.readAllUser()
 
     suspend fun addUser(user: User) {
         userDao.addUser(user)
+    }
+
+    suspend fun updateUser(user: User){
+        userDao.updateUser(user)
     }
 
 }
